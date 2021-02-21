0be72d7420cd4943f2e84080483ef03d8afbd22a
Nikita Ferelin <lizyh112@gmail.com>
2021-02-21 20:01:42 +0200

    Fix

--- remote/src/main/java/com/ferelin/remote/webSocket/WebSocketConnectorHelper.kt
+++ remote/src/main/java/com/ferelin/remote/webSocket/WebSocketConnectorHelper.kt
@@ -1,8 +1,9 @@
 package com.ferelin.remote.webSocket
 
 import com.ferelin.remote.base.BaseResponse
+import com.ferelin.remote.utilits.Api
 import kotlinx.coroutines.flow.Flow
 
 interface WebSocketConnectorHelper {
-    fun openConnection(dataToSubscribe: Collection<String>, token: String): Flow<BaseResponse>
+    fun openConnection(dataToSubscribe: Collection<String>, token: String = Api.FINNHUB_TOKEN): Flow<BaseResponse>
 }
