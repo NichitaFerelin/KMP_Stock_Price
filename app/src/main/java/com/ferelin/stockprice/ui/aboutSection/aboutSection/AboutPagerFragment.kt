726f56ab20cbb78c3d33164a1e3ca9b1e3be48a0
Nikita Ferelin <lizyh112@gmail.com>
2021-04-27 10:37:27 +0300

    ViewPager offscreenPageLimit

--- app/src/main/java/com/ferelin/stockprice/ui/aboutSection/aboutSection/AboutPagerFragment.kt
+++ app/src/main/java/com/ferelin/stockprice/ui/aboutSection/aboutSection/AboutPagerFragment.kt
@@ -179,6 +179,7 @@ class AboutPagerFragment(
             }
         }
         mBinding!!.viewPager.registerOnPageChangeCallback(mViewPagerCallback)
+        mBinding!!.viewPager.offscreenPageLimit = 5
     }
 
     private fun setUpImageListeners() {
