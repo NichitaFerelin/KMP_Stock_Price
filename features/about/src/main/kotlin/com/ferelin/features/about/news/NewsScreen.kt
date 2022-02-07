00f5b698392bd3ab39fcbde4cf1de1ea6e856d8a
Ferelin <lizyh112@gmail.com>
2022-02-07 10:36:25 +0200

    News UI fix

--- features/about/src/main/kotlin/com/ferelin/features/about/news/NewsScreen.kt
+++ features/about/src/main/kotlin/com/ferelin/features/about/news/NewsScreen.kt
@@ -1,17 +1,24 @@
 package com.ferelin.features.about.news
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.animation.Crossfade
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.ferelin.core.domain.entity.LceState
+import com.ferelin.core.ui.R
 import com.ferelin.core.ui.params.NewsParams
+import com.ferelin.core.ui.theme.AppTheme
 import com.ferelin.features.about.uiComponents.NewsItem
 
 @Composable
@@ -35,25 +42,49 @@ private fun NewsScreen(
   uiState: NewsStateUi,
   onUrlClick: (String) -> Unit
 ) {
-  LazyColumn(
-    modifier = Modifier.fillMaxWidth(),
-    contentPadding = PaddingValues(
-      start = 6.dp,
-      end = 6.dp,
-      top = 12.dp,
-      bottom = 70.dp
-    ),
-    verticalArrangement = Arrangement.spacedBy(8.dp)
-  ) {
-    items(items = uiState.news) { newsViewData ->
-      NewsItem(
-        source = newsViewData.source,
-        url = newsViewData.sourceUrl,
-        date = newsViewData.date,
-        title = newsViewData.headline,
-        content = newsViewData.summary,
-        onUrlClick = onUrlClick
-      )
+  Crossfade(targetState = uiState.newsLce) { lceState ->
+    Box(
+      modifier = Modifier
+        .fillMaxSize()
+        .background(AppTheme.colors.backgroundPrimary),
+      contentAlignment = Alignment.Center
+    ) {
+      when (lceState) {
+        is LceState.Content -> {
+          LazyColumn(
+            modifier = Modifier.fillMaxWidth(),
+            contentPadding = PaddingValues(
+              start = 6.dp,
+              end = 6.dp,
+              top = 12.dp,
+              bottom = 70.dp
+            ),
+            verticalArrangement = Arrangement.spacedBy(8.dp)
+          ) {
+            items(items = uiState.news) { newsViewData ->
+              NewsItem(
+                source = newsViewData.source,
+                url = newsViewData.sourceUrl,
+                date = newsViewData.date,
+                title = newsViewData.headline,
+                content = newsViewData.summary,
+                onUrlClick = onUrlClick
+              )
+            }
+          }
+        }
+        is LceState.Loading -> {
+          CircularProgressIndicator(color = AppTheme.colors.contendTertiary)
+        }
+        is LceState.Error -> {
+          Text(
+            text = stringResource(id = R.string.errorDownload),
+            style = AppTheme.typography.body1,
+            color = AppTheme.colors.textPrimary
+          )
+        }
+        else -> Unit
+      }
     }
   }
 }
