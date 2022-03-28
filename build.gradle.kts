1e93e42a7e037afcf9108ab5ecb6289c4666e111
Ferelin <lizyh112@gmail.com>
2022-03-28 17:42:39 +0300

    Version updates

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ buildscript {
     gradlePluginPortal()
   }
   dependencies {
-    classpath("com.android.tools.build:gradle:7.2.0-beta01")
+    classpath("com.android.tools.build:gradle:7.1.2")
     classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:${com.ferelin.Deps.kotlinVersion}")
     classpath("com.google.gms:google-services:4.3.10")
     classpath("com.google.firebase:firebase-crashlytics-gradle:2.8.1")
