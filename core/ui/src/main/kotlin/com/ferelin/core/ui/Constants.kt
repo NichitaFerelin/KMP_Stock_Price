544ca1f4135857088df485e7e9f2c1c645ca838d
NichitaFerelin <68856530+NichitaFerelin@users.noreply.github.com>
2022-04-20 17:18:23 +0300

    App Launcher icon changed

--- core/ui/src/main/kotlin/com/ferelin/core/ui/Constants.kt
+++ core/ui/src/main/kotlin/com/ferelin/core/ui/Constants.kt
@@ -5,3 +5,5 @@ import androidx.compose.ui.unit.dp
 val APP_TOOLBAR_BASELINE = 150.dp
 val HOME_ROUNDED_CORNER = 70.dp
 val APP_CONTENT_PADDING = HOME_ROUNDED_CORNER / 2
+
+const val LOTTIE_SPLASH_ANIM = "lottie_splash.json"
